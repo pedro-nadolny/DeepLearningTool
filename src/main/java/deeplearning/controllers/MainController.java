c8ff89e6055043806e438506f2bac38965fbf822
Pedro Roberto <pedro.nadolny@gmail.com>
2017-09-08 11:41:47 -0300

    Implementação parcial das opções do Deep Learning 2.

--- src/main/java/deeplearning/controllers/MainController.java
+++ src/main/java/deeplearning/controllers/MainController.java
@@ -3,13 +3,11 @@ package deeplearning.controllers;
 import com.jfoenix.controls.*;
 
 import com.sun.javafx.collections.ImmutableObservableList;
-import com.sun.javafx.collections.ObservableListWrapper;
 import deeplearning.models.HistoryRow;
 
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableListBase;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -26,8 +24,8 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
 import org.deeplearning4j.nn.conf.layers.Layer;
+
 import org.deeplearning4j.nn.conf.layers.PoolingType;
-import org.nd4j.linalg.activations.IActivation;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.bayes.NaiveBayes;
@@ -35,19 +33,18 @@ import weka.classifiers.functions.Dl4jMlpClassifier;
 import weka.classifiers.rules.OneR;
 import weka.core.Attribute;
 import weka.core.Instances;
-import weka.core.SelectedTag;
 import weka.core.converters.ConverterUtils.DataSource;
 
 import weka.classifiers.functions.MultilayerPerceptron;
 import weka.classifiers.trees.J48;
-import weka.dl4j.activations.ActivationIdentity;
-import weka.dl4j.layers.BatchNormalization;
+import weka.dl4j.activations.ActivationReLU;
 import weka.dl4j.layers.ConvolutionLayer;
 import weka.dl4j.layers.SubsamplingLayer;
 
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -455,6 +452,7 @@ public class MainController extends VBox implements Initializable{
             c45.setConfidenceFactor(((float)sliderConfidenceFactorC45.getValue())/100);
             c45.setMinNumObj((int)sliderMinLeaftC45.getValue());
             c45.buildClassifier(base);
+
             Evaluation e = new Evaluation(base);
             e.crossValidateModel(c45, base, 10, new Random(1));
             Platform.runLater(() -> finishCCTaskOfIndex(historyIndex, e, c45));
@@ -521,54 +519,48 @@ public class MainController extends VBox implements Initializable{
             Dl4jMlpClassifier DL = new Dl4jMlpClassifier();
 
             int nLayers = (int) sliderNumberOfConvLayersDL.getValue();
-            ArrayList<Layer> layers = new ArrayList<Layer>();
+            List<Layer> layers = new ArrayList<Layer>();
 
             for(int i = 0; i < nLayers; i++) {
+                int[] const22 = {2, 2};
+
                 ConvolutionLayer l = new ConvolutionLayer();
                 l.setLearningRate(sliderLearningRateDL.getValue()/100);
                 l.setMomentum(sliderMomentumDL.getValue()/100);
                 l.setKernelSizeX((int)sliderKernelXDL.getValue());
                 l.setKernelSizeX((int)sliderKernelYDL.getValue());
-                l.setStrideX(2);
-                l.setStrideY(2);
-                l.setPaddingX(2);
-                l.setPaddingY(2);
-                l.setLayerName(i + " Conv Layer");
+                l.setStride(const22);
+                l.setPadding(const22);
+
+
 
                 SubsamplingLayer sb = new SubsamplingLayer();
-                sb.setStrideX(2);
-                sb.setStrideY(2);
-                sb.setKernelSizeX(2);
-                sb.setKernelSizeY(2);
-                sb.setLayerName(i + "Pooling Layer");
-                sb.setActivationFn(new ActivationIdentity());
-                sb.setActivationFunction(new org.nd4j.linalg.activations.impl.ActivationIdentity());
+                sb.setActivationFunction(new ActivationReLU());
+                sb.setStride(const22);
+                sb.setKernelSize(const22);
 
-                System.out.println(poolingTypeComboBox.getValue());
 
-                switch((String)poolingTypeComboBox.getValue()) {
-                    case "MIN":
-                        sb.setPoolingType(PoolingType.SUM);
-                        break;
-
-                    case "AVG":
-                        sb.setPoolingType(PoolingType.AVG);
-                        break;
-
-                    case "PNORM":
-                        sb.setPoolingType(PoolingType.PNORM);
-                        break;
-
-                    case "MAX":
-                        sb.setPoolingType(PoolingType.MAX);
-                        break;
+                if(poolingTypeComboBox.getValue() == "MAX") {
+                    sb.setPoolingType(PoolingType.MAX);
+                } else if(poolingTypeComboBox.getValue() == "SUM") {
+                    sb.setPoolingType(PoolingType.SUM);
+                } else if(poolingTypeComboBox.getValue() == "AVG") {
+                    sb.setPoolingType(PoolingType.AVG);
+                } else if(poolingTypeComboBox.getValue() == "PNORM") {
+                    sb.setPoolingType(PoolingType.PNORM);
                 }
 
                 layers.add(l);
                 layers.add(sb);
             }
 
-            DL.setLayers((Layer[]) layers.toArray());
+            //Keep output layer
+            layers.add(DL.getLayers()[0]);
+
+            Layer[] allLayers = new Layer[layers.size()];
+            allLayers = layers.toArray(allLayers);
+
+            DL.setLayers(allLayers);
             DL.setNumEpochs((int)sliderNumberOfEpochsDL.getValue());
 
             DL.buildClassifier(base);
@@ -576,6 +568,7 @@ public class MainController extends VBox implements Initializable{
             e.crossValidateModel(DL, base, 10, new Random(1));
             Platform.runLater(() -> finishDLTaskOfIndex(historyIndex, e, DL));
         } catch(Exception _) {
+            _.printStackTrace();
             Platform.runLater(() -> finishTaskOfIndexWithError(historyIndex));
         }
     }
